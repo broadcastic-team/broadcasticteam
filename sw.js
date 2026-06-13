448343ff74a4279162de4035e3d010d3b2f8f400
broadcastic-team <mohammadabbasi.music@gmail.com>
2026-06-13 04:16:17 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,6 +2,8 @@ const CACHE_NAME = 'broadcastic-v1';
 const ASSETS = [
   '/',
   '/index.html',
+  '/manifest.json',
+  '/favicon.png',
   '/logo.png',
   '/header.jpg',
   '/card1.png',
@@ -9,20 +11,31 @@ const ASSETS = [
   '/card3.png',
   '/card4.png',
   '/midder.jpg',
-  '/footer_logo.jpg',
-  '/instagram.svg',
-  '/telegram.svg',
-  '/manifest.json'
+  '/footer_logo.jpg'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
+  self.clients.claim();
 });
